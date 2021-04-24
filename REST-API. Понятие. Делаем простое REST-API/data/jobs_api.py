09c1d5801f7cf18bd4680e7bc851e301c9146a7e
Иванчик <peregh320@gmail.com>
2021-04-24 22:23:58 +0300

    _

--- REST-API. Понятие. Делаем простое REST-API/data/jobs_api.py
+++ REST-API. Понятие. Делаем простое REST-API/data/jobs_api.py
@@ -37,22 +37,39 @@ def get_one_jobs(job_id):
 @blueprint.route('/api/jobs', methods=['POST'])
 def create_jobs():
     db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).filter(Jobs.id == request.json['id']).first()
     if not request.json:
         return jsonify({'error': 'Empty request'})
     elif not all(key in request.json for key in
                  ['id', 'job', 'work_size', 'collaborators',
                   'is_finished', 'team_leader']):
         return jsonify({'error': 'Bad request'})
-    elif request.json['id'] in [i.id for i in db_sess.query(Jobs).all()]:
-        return jsonify({'error': 'Id already exists'})
-    job = Jobs(
-        id=request.json['id'],
-        job=request.json['job'],
-        work_size=request.json['work_size'],
-        collaborators=request.json['collaborators'],
-        is_finished=request.json['is_finished'],
-        team_leader=request.json['team_leader'],
-    )
-    db_sess.add(job)
+    if jobs is not None:
+        jobs.job = request.json['job']
+        jobs.work_size = request.json['work_size']
+        jobs.collaborators = request.json['collaborators']
+        jobs.is_finished = request.json['is_finished']
+        jobs.team_leader = request.json['team_leader']
+    else:
+        job = Jobs(
+            id=request.json['id'],
+            job=request.json['job'],
+            work_size=request.json['work_size'],
+            collaborators=request.json['collaborators'],
+            is_finished=request.json['is_finished'],
+            team_leader=request.json['team_leader'],
+        )
+        db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+@blueprint.route('/api/jobs/<int:job_id>', methods=['DELETE'])
+def delete_job(job_id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).get(job_id)
+    if not job:
+        return jsonify({'error': 'Not found'})
+    db_sess.delete(job)
     db_sess.commit()
     return jsonify({'success': 'OK'})
--- REST-API. Понятие. Делаем простое REST-API/data/test.py
+++ REST-API. Понятие. Делаем простое REST-API/data/test.py
@@ -1,36 +1,41 @@
-from requests import get, post
+from requests import get, post, delete
 
 # Посмотрим все работы до внесения изменений в таблицу
 print(get('http://localhost:5000/api/jobs').json())
 
 # (Конкретный запрос) Добавляем работу с id, равным 18
 print(post('http://localhost:5000/api/jobs', json={
-        'id': 18,
-        'job': 'Joke job',
-        'work_size': 3,
-        'collaborators': '1',
-        'is_finished': True,
-        'team_leader': 1}).json())
+    'id': 16,
+    'job': 'Joke job',
+    'work_size': 3,
+    'collaborators': '1',
+    'is_finished': True,
+    'team_leader': 1}).json())
 
 # (Неконкретный запрос) отсутствует параметр is_finished
 print(post('http://localhost:5000/api/jobs', json={
-        'id': 13,
-        'job': 'Joke job',
-        'work_size': 3,
-        'collaborators': '1',
-        'team_leader': 1}).json())
+    'id': 16,
+    'job': 'Joke job',
+    'work_size': 3,
+    'collaborators': '1',
+    'team_leader': 1}).json())
 
 # (Неконкретный запрос) Пустой запрос
 print(post('http://localhost:5000/api/jobs').json())
 
 # (Неконкретный запрос) Уже добавлена работы с таким id
 print(post('http://localhost:5000/api/jobs', json={
-        'id': 18,
-        'job': 'Joke job',
-        'work_size': 3,
-        'collaborators': '1',
-        'is_finished': True,
-        'team_leader': 1}).json())
+    'id': 18,
+    'job': 'Joke job',
+    'work_size': 3,
+    'collaborators': '1',
+    'is_finished': True,
+    'team_leader': 1}).json())
+
+# Посмотрим на словарь работ после внесения изменений
+print(get('http://localhost:5000/api/jobs').json())
+
+print(delete('http://localhost:5000/api/jobs/16').json())
 
 # Посмотрим на словарь работ после внесения изменений
 print(get('http://localhost:5000/api/jobs').json())
